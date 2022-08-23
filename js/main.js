d746108f3ef12923794d46efb3e8250af8c70312
L&F <luis.oviedo@novasec.co>
2022-08-23 09:48:37 -0500

    update carpetas de estilos

---
+++ js/main.js
@@ -0,0 +1,31 @@
+/*===== FOCUS =====*/
+const inputs = document.querySelectorAll(".form__input")
+
+/*=== Add focus ===*/
+function addfocus(){
+    let parent = this.parentNode.parentNode
+    parent.classList.add("focus")
+}
+
+/*=== Remove focus ===*/
+function remfocus(){
+    let parent = this.parentNode.parentNode
+    if(this.value == ""){
+        parent.classList.remove("focus")
+    }
+}
+
+/*=== To call function===*/
+inputs.forEach(input=>{
+    input.addEventListener("focus",addfocus)
+    input.addEventListener("blur",remfocus)
+})
+
+// sweet alert boton
+$('#leerMas').click(function(){
+    Swal.fire({
+       icon: 'error',
+       title: 'Oops...',
+       text: 'Lo sentimos aun no tenemos la tienda activa \n Notificaremos cuando este lista!',
+    })
+ });
